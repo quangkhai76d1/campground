b6ef72a40e51f9dc9cfc4614e96b8e26a74a19ca
Quang Khai <18520876@gm.uit.edu.vn>
2021-07-11 15:30:44 +0700

    Add database for campground

--- models/campground.js
+++ models/campground.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
-const Schema = require("Schema");
+const Schema = mongoose.Schema;
 
-const CampgroundSchema=new  Schema({
-    
-})
+const CampgroundSchema = new Schema({
+    title: String,
+    image: String,
+    price: Number,
+    description: String,
+    location: String,
+});
+
+module.exports = mongoose.model("Campground", CampgroundSchema);
